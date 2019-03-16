ef9324df06d4ca2883f766f780dcca78f5672089
danielnwaroh <daniel.nwaroh@ucalgary.ca>
2019-03-16 10:55:28 -0600

    Some Front End Stuff, code is in src folder
    coded in the app.js file

---
+++ Shuffl/build/precache-manifest.4338ae9f882c19f2e36facd1470fec19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "41fe4597139dbc2ffffa",
+    "url": "/static/js/main.7f5a412d.chunk.js"
+  },
+  {
+    "revision": "524cdb34032e64a82444",
+    "url": "/static/js/2.ce32ba7a.chunk.js"
+  },
+  {
+    "revision": "41fe4597139dbc2ffffa",
+    "url": "/static/css/main.92487e05.chunk.css"
+  },
+  {
+    "revision": "3ca670ec254ce99c56e5dc3f1475e3fe",
+    "url": "/index.html"
+  }
+];
