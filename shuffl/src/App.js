86ac7cd0fe90d67a9dc2f213a15a6381b2720ad6
arthurjason08 <arthurjason08@gmail.com>
2019-03-27 13:43:04 -0600

    updated signUp logIn js

--- shuffl/src/App.js
+++ shuffl/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
         <Router>
-            <Route path={"/"} exact component={Homepage} />
+            <Route path={"/"} exact component={LogIn} />
         </Router>
     );
   }
