dc717a53c8e6501ab9dc71a55f1d6e8c3beadc83
arthurjason08 <arthurjason08@gmail.com>
2019-03-29 22:12:31 -0600

    Fixed component structure

---
+++ shuffl/src/UnusedComponents/Room.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Room extends Component {
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+            // should take in props dynamically (image, chatroom name)
+            )
+    }
+}
+
+export default Room
---
+++ shuffl/src/UnusedComponents/Username.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Username extends Component {
+
+    render() {
+        return(
+            <div>
+
+            </div>
+            // should take in name prop dynamically
+            )
+    }
+}
+
+export default Username
