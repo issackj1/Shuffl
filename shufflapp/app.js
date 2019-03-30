22d3758983d91b625b24b3d468bfd6f28c04c98f
issackj1 <issack.john1@ucalgary.ca>
2019-03-30 16:29:29 -0600

    changes to shufflapp
    reformatted apps

--- shufflapp/app.js
+++ shufflapp/app.js
@@ -3,18 +3,19 @@ const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const flash = require('connect-flash');
-const session = require('express-session'); 
-const app = express();
+const session = require('express-session');
 const passport = require('passport');
-const PORT = 4000;
 const bodyParser = require('body-parser');
-const shufflRoutes = express.Router();
+const PORT = 4000;
+const app = express();
+const router = express.Router();
+
 
 app.use(bodyParser.json());
 // Routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
-app.use('/chatrooms', shufflRoutes);
+app.use('/chatrooms', router);
 
 //passport config
 require('./config/passport')(passport);
@@ -65,7 +66,7 @@ app.use((req, res, next) => {
 });
 
 //display all the Available to join chatrooms
-shufflRoutes.route('/').get(function (req, res) {
+router.get('/', function (req, res) {
     ChatRoom.find(function (err, chatrooms) {
         if (err){
             console.log(err)
@@ -76,7 +77,7 @@ shufflRoutes.route('/').get(function (req, res) {
 });
 
 //display a specific chatroom
-shufflRoutes.route('/:id').get(function (req, res) {
+router.get('/:id', function (req, res) {
     let id = req.params.id;
     ChatRoom.findById(id, function (err, chatroom) {
         if (err){
@@ -88,7 +89,7 @@ shufflRoutes.route('/:id').get(function (req, res) {
 });
 
 //create a chatroom and add to the database
-shufflRoutes.route('/add').post(function (req, res) {
+router.post('/add', function (req, res) {
     let chatroom = new ChatRoom(req.body);
     chatroom.save()
         .then(chatroom =>{
@@ -100,7 +101,7 @@ shufflRoutes.route('/add').post(function (req, res) {
 });
 
 //update a specific chatroom
-shufflRoutes.route('/update/:id').post(function (req, res) {
+router.post('/update/:id', function (req, res) {
     ChatRoom.findById(req.params.id, function (err, chatroom) {
         if (!chatroom){
             res.status(404).send('Could not find chatroom');
@@ -129,7 +130,7 @@ shufflRoutes.route('/update/:id').post(function (req, res) {
 });
 
 //add a song to the queue
-shufflRoutes.route('/addsong/:id').post(function (req, res) {
+router.post('/addsong/:id', function (req, res) {
     ChatRoom.findById(req.params.id, function (err, chatroom) {
         if (!chatroom){
             res.status(404).send('Could not find chatroom');
