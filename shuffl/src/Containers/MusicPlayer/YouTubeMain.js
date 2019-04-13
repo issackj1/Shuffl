34997801f55b92eda80288a755a0f2eed5530b0c
ajsantos08 <arthurjason08@gmail.com>
2019-04-13 15:21:30 -0600

    bug fixes

--- shuffl/src/Containers/MusicPlayer/YouTubeMain.js
+++ shuffl/src/Containers/MusicPlayer/YouTubeMain.js
@@ -2,12 +2,11 @@ import React, { Component } from 'react';
 import SearchBar from './search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './video_list';
-import Queue from './video_list_guest'
+import Queue from './video_list_guest';
 import '../../css/TempYouTube.css';
 import YouTube from 'react-youtube';
 import Player from '../../Components/Player';
-import Chat from '../../Components/Chat'
-
+import Chat from '../../Components/Chat';
 
 const API_KEY = 'AIzaSyBdVut9QCzqAHBzfDEh30yUp4E529som6s';
 
@@ -18,70 +17,68 @@ class YouTubeMain extends Component {
 		this.state = {
 			search: false,
 			player: null,
-			videos: [],	
+			videos: [],
 			videoId: '',
 			queue: [],
 			chat: false,
-			messages:[]
+			messages: []
 		};
 	}
 
 	pauseVideo = () => {
 		if (this.state.player.getPlayerState() == 1) {
 			this.state.player.pauseVideo();
-			this.props.socket.emit('sendpause', this.props.RoomId)
+			this.props.socket.emit('sendpause', this.props.RoomId);
 		} else {
 			this.state.player.playVideo();
-			this.props.socket.emit('sendplay', this.props.RoomId)
+			this.props.socket.emit('sendplay', this.props.RoomId);
 		}
 	};
 
 	addToQueue = (video) => {
-		
-		if((this.state.queue.length>=0) && (this.state.player.getPlayerState() !== 5)){
+		if (this.state.queue.length >= 0 && this.state.player.getPlayerState() !== 5) {
 			this.setState((prevState) => ({
 				queue: [ ...prevState.queue, video ]
 			}));
-		}else{
-			this.state.player.loadVideoById(video.id.videoId, 0, 'large')
-			this.setState({videoId:video.id.videoId})
-			this.props.socket.emit('updateVideo', this.props.RoomId, video.id.videoId)
+		} else {
+			this.state.player.loadVideoById(video.id.videoId, 0, 'large');
+			this.setState({ videoId: video.id.videoId });
+			this.props.socket.emit('updateVideo', this.props.RoomId, video.id.videoId);
 		}
 	};
 
-	chat = ()=>{	
-		this.setState({chat: !this.state.chat})
-		
-	}
+	chat = () => {
+		this.setState({ chat: !this.state.chat });
+	};
 
 	searchVideo = () => {
-		this.setState({ search: !this.state.search })
-		
-	
+		this.setState({ search: !this.state.search });
 	};
 
 	skipVideo = () => {
-		if(this.state.queue.length >=1){
-			this.state.player.loadVideoById(this.state.queue[0].id.videoId,0,'large')
-			this.setState({videoId: this.state.queue[0].id.videoId, queue:this.state.queue.slice(1)})
-			this.props.socket.emit('sendskip', this.props.RoomId)
-		}else{
-			this.state.player.stopVideo()
-			this.props.socket.emit('sendstop', this.props.RoomId)
+		if (this.state.queue.length >= 1) {
+			this.state.player.loadVideoById(this.state.queue[0].id.videoId, 0, 'large');
+			this.setState({ videoId: this.state.queue[0].id.videoId, queue: this.state.queue.slice(1) });
+			this.props.socket.emit('sendskip', this.props.RoomId);
+		} else {
+			this.state.player.stopVideo();
+			this.props.socket.emit('sendstop', this.props.RoomId);
 		}
-		
+
 		// var time = this.state.player.getCurrentTime();
 		// this.state.player.seekTo(time + 10, true);
 	};
 
-
-	initChat=()=>{
-		this.props.socket.on('receivemessage', function (msg) {
-			this.setState({
-				messages : this.state.messages.concat([msg])
-			});
-		}.bind(this))		
-	}
+	initChat = () => {
+		this.props.socket.on(
+			'receivemessage',
+			function(msg) {
+				this.setState({
+					messages: this.state.messages.concat([ msg ])
+				});
+			}.bind(this)
+		);
+	};
 
 	//TODO add function to send queued video to mongo
 	componentDidMount() {
@@ -95,33 +92,34 @@ class YouTubeMain extends Component {
 		// if it's video id then json.stringify each prevState.queue and this.state.queue and compare
 
 		if (prevState.queue !== this.state.queue) {
-			this.props.socket.emit('updateQueue', this.props.RoomId, this.state.queue)
+			this.props.socket.emit('updateQueue', this.props.RoomId, this.state.queue);
 		}
 
-		if(prevProps.RoomId != this.props.RoomId){
-			this.setState({queue:[], messages:[], videos: [],videoId: ''})
-			this.state.player.stopVideo()
-		}
-
-		if(prevState.player !== this.state.player){
-			this.state.player.setVolume(100)
-		}
-
-		if(prevState.search !== this.state.search){
-			if(this.state.chat){
-				let chat = document.getElementsByClassName("chat")[0];
-				if (this.state.search) {
-					chat.style.height = "61vh";
-				} else {
-					chat.style.height = "83vh";
-				}
+		if (prevProps.RoomId != this.props.RoomId) {
+			this.setState({ queue: [], messages: [], videos: [], videoId: '' });
+			if (this.state.player) {
+				this.state.player.stopVideo();
 			}
 		}
 
+		if (prevState.player !== this.state.player) {
+			this.state.player.setVolume(100);
+		}
+
+		if (prevState.search !== this.state.search) {
+			if (this.state.chat) {
+				let chat = document.getElementsByClassName('chat')[0];
+				if (this.state.search) {
+					chat.style.height = '61vh';
+				} else {
+					chat.style.height = '83vh';
+				}
+			}
+		}
 	}
 
 	videoSearch(searchTerm) {
-		YTSearch({ key: API_KEY, term: searchTerm+"Official Audio", maxResults: 10}, (data) => {
+		YTSearch({ key: API_KEY, term: searchTerm + 'Official Audio', maxResults: 10 }, (data) => {
 			this.setState({ videos: data });
 			// this.setState({ videoId: data[0].id.videoId });
 		});
@@ -131,15 +129,22 @@ class YouTubeMain extends Component {
 	handleReady = (e) => {
 		this.setState({ player: e.target });
 
-		this.props.socket.on('timereq', (username)=>{
-			console.log(this.state.videoId)
-			this.props.socket.emit('sendtime', this.props.RoomId, username, this.state.player.getCurrentTime(), this.state.player.getPlayerState(), this.state.videoId)
-		})
+		this.props.socket.on('timereq', (username) => {
+			console.log(this.state.videoId);
+			this.props.socket.emit(
+				'sendtime',
+				this.props.RoomId,
+				username,
+				this.state.player.getCurrentTime(),
+				this.state.player.getPlayerState(),
+				this.state.videoId
+			);
+		});
 
-		this.props.socket.on('queuereq', (username)=>{
-			console.log()
-			this.props.socket.emit('sendqueue', this.props.RoomId, username, this.state.queue)
-		})
+		this.props.socket.on('queuereq', (username) => {
+			console.log();
+			this.props.socket.emit('sendqueue', this.props.RoomId, username, this.state.queue);
+		});
 	};
 
 	// getCurrentTime =()=>{
@@ -147,7 +152,7 @@ class YouTubeMain extends Component {
 	// 		return this.state.player.getCurrentTime()
 	// 	}else{
 	// 		return 0
-	// 	}	
+	// 	}
 	// }
 
 	render() {
@@ -164,23 +169,34 @@ class YouTubeMain extends Component {
 		return (
 			<div className="controlsShift">
 				<div className="youtubeIframe">
-					<YouTube videoId={this.state.videoId} opts={opts} onReady={this.handleReady}/>
+					<YouTube videoId={this.state.videoId} opts={opts} onReady={this.handleReady} />
 				</div>
-				<Player  pauseVideo={this.pauseVideo} skipVideo={this.skipVideo} searchVideo={this.searchVideo} chat = {this.chat}/>
+				<Player
+					pauseVideo={this.pauseVideo}
+					skipVideo={this.skipVideo}
+					searchVideo={this.searchVideo}
+					chat={this.chat}
+				/>
 				{this.state.search ? (
 					<div className="footerGrey">
 						<SearchBar onSearchTermChange={(searchTerm) => this.videoSearch(searchTerm)} />
 						<div className="videoListContainer">
-						<VideoList videos={this.state.videos} addToQueue={this.addToQueue} />
+							<VideoList videos={this.state.videos} addToQueue={this.addToQueue} />
 						</div>
 					</div>
-				) :<Queue videos={this.state.queue} />}
-				{
-					this.state.chat?(
-						<Chat socket={this.props.socket}sendMessage={this.sendMessage}
-						messages={this.state.messages} users = {this.state.users} Username={this.props.Username} RoomId={this.props.RoomId}/>
-					):null
-				}
+				) : (
+					<Queue videos={this.state.queue} />
+				)}
+				{this.state.chat ? (
+					<Chat
+						socket={this.props.socket}
+						sendMessage={this.sendMessage}
+						messages={this.state.messages}
+						users={this.state.users}
+						Username={this.props.Username}
+						RoomId={this.props.RoomId}
+					/>
+				) : null}
 			</div>
 		);
 	}
--- shuffl/src/Containers/MusicPlayer/YoutubeMainGuest.js
+++ shuffl/src/Containers/MusicPlayer/YoutubeMainGuest.js
@@ -4,7 +4,7 @@ import '../../css/TempYouTube.css';
 import YouTube from 'react-youtube';
 import PlayerGuest from '../../Components/PlayerGuest';
 import Chat from '../../Components/Chat';
-import VideoList from '../MusicPlayer/video_list_guest'
+import VideoList from '../MusicPlayer/video_list_guest';
 
 const API_KEY = 'AIzaSyBdVut9QCzqAHBzfDEh30yUp4E529som6s';
 
@@ -18,63 +18,89 @@ class YouTubeMainGuest extends Component {
 			player: null,
 			videoId: '',
 			queueguest: [],
-			messagesguest:[]
+			messagesguest: []
 		};
 	}
-	
+
 	//TODO add function to send queued video to mongo
 	componentDidMount() {
-		this.props.socket.emit('reqqueue', this.props.RoomId, this.props.Username)
+		this.props.socket.emit('reqqueue', this.props.RoomId, this.props.Username);
 
-		this.props.socket.on('receivemessage', function (msg) {
-			this.setState({
-				messagesguest : this.state.messagesguest.concat([msg])
-			});
-		}.bind(this))	
-		
-        this.props.socket.on('receiveplay', function(){
-            this.state.player.playVideo();
-        }.bind(this))
+		this.props.socket.on(
+			'receivemessage',
+			function(msg) {
+				this.setState({
+					messagesguest: this.state.messagesguest.concat([ msg ])
+				});
+			}.bind(this)
+		);
 
-        this.props.socket.on('receivepause', function(){
-            this.state.player.pauseVideo();
-		}.bind(this))
+		this.props.socket.on(
+			'receiveplay',
+			function() {
+				this.state.player.playVideo();
+			}.bind(this)
+		);
 
-		this.props.socket.on('receivestop', function(){
-            this.state.player.stopVideo();
-		}.bind(this))
-		
-		this.props.socket.on('receiveskip', function(){
-				this.state.player.loadVideoById(this.state.queueguest[0].id.videoId ,0,'large')
-				this.setState({queueguest:this.state.queueguest.slice(1)})
-        }.bind(this))
+		this.props.socket.on(
+			'receivepause',
+			function() {
+				this.state.player.pauseVideo();
+			}.bind(this)
+		);
 
-        this.props.socket.on('receivetime', function(username, time, state, videoid){
-			if(this.props.Username === username){
-				console.log(videoid)
-				if(state == 1){
-					this.state.player.loadVideoById(videoid, time,'large')
-				}else{
-					this.state.player.cueVideoById(videoid, time, 'large')
+		this.props.socket.on(
+			'receivestop',
+			function() {
+				this.state.player.stopVideo();
+			}.bind(this)
+		);
+
+		this.props.socket.on(
+			'receiveskip',
+			function() {
+				this.state.player.loadVideoById(this.state.queueguest[0].id.videoId, 0, 'large');
+				this.setState({ queueguest: this.state.queueguest.slice(1) });
+			}.bind(this)
+		);
+
+		this.props.socket.on(
+			'receivetime',
+			function(username, time, state, videoid) {
+				if (this.props.Username === username) {
+					console.log(videoid);
+					if (state == 1) {
+						this.state.player.loadVideoById(videoid, time, 'large');
+					} else {
+						this.state.player.cueVideoById(videoid, time, 'large');
+					}
 				}
-			}
-		}.bind(this))
-		
-		this.props.socket.on('receivequeue', function(username, queue){
-			if(this.props.Username === username){
-				this.setState({queueguest:queue})
-			}
-		}.bind(this))
-		
-		this.props.socket.on('updateQueue', function(queuelist){
-			this.setState({queueguest:queuelist})
-		}.bind(this))
+			}.bind(this)
+		);
 
-		this.props.socket.on('updateVideo', function(videoid){
-			this.state.player.loadVideoById(videoid, 0,'large')
-		}.bind(this))
+		this.props.socket.on(
+			'receivequeue',
+			function(username, queue) {
+				if (this.props.Username === username) {
+					this.setState({ queueguest: queue });
+				}
+			}.bind(this)
+		);
 
-    }
+		this.props.socket.on(
+			'updateQueue',
+			function(queuelist) {
+				this.setState({ queueguest: queuelist });
+			}.bind(this)
+		);
+
+		this.props.socket.on(
+			'updateVideo',
+			function(videoid) {
+				this.state.player.loadVideoById(videoid, 0, 'large');
+			}.bind(this)
+		);
+	}
 
 	componentDidUpdate(prevProps, prevState) {
 		// queue is object
@@ -83,30 +109,30 @@ class YouTubeMainGuest extends Component {
 		// if it's video id then json.stringify each prevState.queue and this.state.queue and compare
 
 		if (prevState.queueguest !== this.state.queueguest) {
-			console.log(this.state.queueguest)
-				}
-				
-    if(prevState.player !== this.state.player){
-			this.state.player.setVolume(100)
-			this.props.socket.emit('reqtime', this.props.RoomId, this.props.Username)
-			this.props.socket.emit('reqqueue', this.props.RoomId, this.props.Username)
+			console.log(this.state.queueguest);
 		}
 
-		if(prevProps.RoomId != this.props.RoomId){
-			this.setState({queueguest:[], messagesguest:[], videoId:''})
-			this.state.player.stopVideo()
+		if (prevState.player !== this.state.player) {
+			this.state.player.setVolume(100);
+			this.props.socket.emit('reqtime', this.props.RoomId, this.props.Username);
+			this.props.socket.emit('reqqueue', this.props.RoomId, this.props.Username);
 		}
 
+		if (prevProps.RoomId != this.props.RoomId) {
+			this.setState({ queueguest: [], messagesguest: [], videoId: '' });
+			if (this.state.player) {
+				this.state.player.stopVideo();
+			}
+		}
+	}
 
-    }
-	
 	queue = () => {
 		this.setState({ queuebutton: !this.state.queuebutton });
 	};
 
-	chat = ()=>{
-		this.setState({chat: !this.state.chat})
-	}
+	chat = () => {
+		this.setState({ chat: !this.state.chat });
+	};
 	// console.log(selectedVideo);
 	handleReady = (e) => {
 		this.setState({ player: e.target });
@@ -131,16 +157,20 @@ class YouTubeMainGuest extends Component {
 				{this.state.queuebutton ? (
 					<div className="footerGrey">
 						<div className="videoListContainer">
-						<VideoList videos={this.state.queueguest} />
+							<VideoList videos={this.state.queueguest} />
 						</div>
 					</div>
 				) : null}
-				{
-					this.state.chat?(
-						<Chat socket={this.props.socket}sendMessage={this.sendMessage}
-						messages={this.state.messagesguest} users = {this.state.users} Username={this.props.Username} RoomId={this.props.RoomId}/>
-					):null
-				}
+				{this.state.chat ? (
+					<Chat
+						socket={this.props.socket}
+						sendMessage={this.sendMessage}
+						messages={this.state.messagesguest}
+						users={this.state.users}
+						Username={this.props.Username}
+						RoomId={this.props.RoomId}
+					/>
+				) : null}
 			</div>
 		);
 	}
